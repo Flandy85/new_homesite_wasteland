82e394e892915e3a78a9ae490afb61b499d94192
Anders Gustavsson <gustavsson09@hotmail.com>
2017-10-18 21:40:40 +0200

    Created new files colors.scss and header.scss. Added html structure in index.php and header.php. Added more css in base, main, header and style.scss

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <!--Include header.php once -->
 <?php include_once "./templates/header.php" ?>
-<main>
-	<div class="section-start" id="Start">
-		<h1>WASTELANDSOLUTION</h1>
+<div class="section-main">
+		<div class="section-start" id="Start">
+			<h1>WASTELANDSOLUTION</h1>
+		</div>
+		<div class="background-section">
+			<div class="section-about" id="About">
+				<h2>About</h2>
+				<p>Hi, I’m Anders Gustavsson. This is my portfolio where i share all of my projects. When im not designing web pages in my free time, im studying for Front End Developer. Outside of school and work i like to, playing video games, work out at the gym, hanging out with my friends, eat good food. I also like to make a trip atleast once a year. My first year in school is now complete and in september i will begin my intership as a Front End Developer. So far in school I have completed classes in HTML & CSS, PHP programming, MySQL, graffic designer tool: Photoshop and JavaScript.</p>
+				<img src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-1/p160x160/12295319_10153062929801503_2639753087132379632_n.jpg?oh=100d6b2be784b62b9a56ac45b59cf65a&oe=59FCDA6C">
+			</div>
+			<div class="section-contact" id="Contact">
+				<h2>Contact information</h2>
+				<p class="contact-paragraph1">I am looking for work after i have finished my studies.<br> But if you wish to contact me for a private project let me know. </p>
+
+				<p class="contact-paragraph2">You can reach me either by phone or email.<br> You can also use the contact form.</p>
+				<div class="contact-container">
+					<ul class="contact-list">
+						<li class="contact-item"><a href="tel:0768505328">Tel: 0768505328</a></li>
+						<li class="contact-item"><a href="mailto: gustavsson09@hotmail.com">E-mail: gustavsson09@hotmail.com</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</div>
 </main>
 
--- templates/header.php
+++ templates/header.php
@@ -6,13 +6,15 @@
     <meta name="keywords" content="HTML,CSS,JavaScript">
     <meta name="author" content="Anders Gustavsson">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>wastelandsolution</title>
+    <link href="https://fonts.googleapis.com/css?family=Bowlby+One+SC" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="./dist/css/style.css">
 </head>
 <body>
-<header>
-	<h1>WASTELANDSOLUTION</h1>
-	<nav>
+<header class="header-container">
+	<nav class="menu-container">
+		<h1>WASTELANDSOLUTION</h1>
 		<div class="menu" id="menu-start"><a href="#Start">Start</a></div>
 		<div class="menu"><a href="#About" >About</a></div>
 		<div class="menu"><a href="#Contact">Contact</a></div>

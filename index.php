d1c913a430539e12f928bde2776ff3dea180265d
Anders Gustavsson <gustavsson09@hotmail.com>
2017-10-18 19:08:23 +0200

    Created a new scss file main.scss and added 2 images in folder assets/images

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
-<?php include "./templates/header.php" ?>
+<!--Include header.php once -->
+<?php include_once "./templates/header.php" ?>
+<main>
+	<div class="section-start" id="Start">
+		<h1>WASTELANDSOLUTION</h1>
+	</div>
+</main>
 
 
 
 
-<?php include "./templates/footer.php" ?>
+<?php include_once "./templates/footer.php" ?>
+<!--Include footer.php once -->

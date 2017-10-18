dbdac9224cb2f9337b3dd7b6dad3d531a060de04
Anders Gustavsson <gustavsson09@hotmail.com>
2017-10-18 18:47:53 +0200

    Fixed include in index.php. Tested css in base.scss. Added meta tags and nav bar in header.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php include "header.php" ?>
+<?php include "./templates/header.php" ?>
 
 
 
 
-<?php include "footer.php" ?>
+<?php include "./templates/footer.php" ?>
--- templates/header.php
+++ templates/header.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+	<meta charset="UTF-8">
+    <meta name="description" content="Personal portfolio site">
+    <meta name="keywords" content="HTML,CSS,JavaScript">
+    <meta name="author" content="Anders Gustavsson">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>wastelandsolution</title>
-
+	<link rel="stylesheet" type="text/css" href="./dist/css/style.css">
 </head>
 <body>
 <header>
 	<h1>WASTELANDSOLUTION</h1>
 	<nav>
-		
+		<div class="menu" id="menu-start"><a href="#Start">Start</a></div>
+		<div class="menu"><a href="#About" >About</a></div>
+		<div class="menu"><a href="#Contact">Contact</a></div>
+		<div class="menu"><a href="#">Projects</a></div>
 	</nav>
 </header>

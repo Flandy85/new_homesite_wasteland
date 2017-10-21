7521108072101582175deee2ec0ddbd8956dc182
Anders Gustavsson <gustavsson09@hotmail.com>
2017-10-21 22:42:14 +0200

    Change structure in index.php and change and added css in colors,main, tablets.scss

--- index.php
+++ index.php
@@ -6,23 +6,40 @@
 			<h1>WASTELANDSOLUTION</h1>
 		</div>
 		<div class="background-section">
-			<div class="section-about" id="About">
+			<div class="section-about-top-border">
 				<h2>About</h2>
-				<p>Hi, I’m Anders Gustavsson. This is my portfolio where i share all of my projects. When im not designing web pages in my free time, im studying for Front End Developer. Outside of school and work i like to, playing video games, work out at the gym, hanging out with my friends, eat good food. I also like to make a trip atleast once a year. My first year in school is now complete and in september i will begin my intership as a Front End Developer. So far in school I have completed classes in HTML & CSS, PHP programming, MySQL, graffic designer tool: Photoshop and JavaScript.</p>
-				<img src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-1/p160x160/12295319_10153062929801503_2639753087132379632_n.jpg?oh=100d6b2be784b62b9a56ac45b59cf65a&oe=59FCDA6C">
 			</div>
-			<div class="section-contact" id="Contact">
-				<h2>Contact information</h2>
-				<p class="contact-paragraph1">I am looking for work after i have finished my studies.<br> But if you wish to contact me for a private project let me know. </p>
+			<div class="section-about" id="About">
+				<div class="section-about-text">
+					<p>Hi, I’m Anders Gustavsson. This is my portfolio where i share all of my projects. When im not designing web pages in my free time, im studying for Front End Developer. Outside of school and work i like to, playing video games, work out at the gym, hanging out with my friends, eat good food. I also like to make a trip atleast once a year. My first year in school is now complete and have now started my intership as a Front End Developer.</p>
 
-				<p class="contact-paragraph2">You can reach me either by phone or email.<br> You can also use the contact form.</p>
-				<div class="contact-container">
-					<ul class="contact-list">
-						<li class="contact-item"><a href="tel:0768505328">Tel: 0768505328</a></li>
-						<li class="contact-item"><a href="mailto: gustavsson09@hotmail.com">E-mail: gustavsson09@hotmail.com</a></li>
-					</ul>
+					<p>So far in school I have completed classes in HTML & CSS, PHP programming, MySQL, graffic designer tool: Photoshop and JavaScript.</p>
+
+					<p>Right now im doing my first intership at a financial company called Relenda AB as a <a href="https://angular.io/guide/quickstart">Angular developer.</a> So far our team has developt a beta plattform which contains services such as to be able to sell your own and finance others eDI-invoices.</p>
+
+					<p>Other assignments i have been tasked with are for example: design and create the companys new logo for the new plattform and also designed a product sheet for plattforms services with Photoshop.</p>
 				</div>
+				<img src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-1/p160x160/12295319_10153062929801503_2639753087132379632_n.jpg?oh=100d6b2be784b62b9a56ac45b59cf65a&oe=59FCDA6C">
+				
 			</div>
+			<div class="section-about-top-border">
+				<h2>Contact information</h2>
+			</div>
+			<div class="contact-section-background">
+				<div class="section-contact" id="Contact">
+					<div class="contact-info-container">
+						<p class="contact-paragraph1">I am looking for work after i have finished my studies.<br> But if you wish to contact me for a private project let me know. </p>
+
+						<p class="contact-paragraph2">You can reach me either by phone or email.<br> You can also use the contact form.</p>
+						<div class="contact-container">
+							<ul class="contact-list">
+								<li class="contact-item"><a href="tel:0768505328">Tel: 0768505328</a></li>
+								<li class="contact-item"><a href="mailto: gustavsson09@hotmail.com">E-mail: gustavsson09@hotmail.com</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>	
 		</div>
 	</div>
 </main>

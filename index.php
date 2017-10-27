133c2380e6dc008af1dfcfb45ca08fe715134057
Anders Gustavsson <gustavsson09@hotmail.com>
2017-10-27 22:34:46 +0200

    More changes in index.php and in colors, desktop andmainscss

--- index.php
+++ index.php
@@ -6,105 +6,135 @@
 			<h1>ANDERS GUSTAVSSON</h1>
 			<h2>FRONTEND DEVOLOPER</h2>
 		</div>
-		<div class="background-section">
-			<!-- <div class="section-about-top-border" id="About"> -->
-				
-			<!-- </div> -->
-			<div class="section-about" id="About">
-				<img src="./assets/images/rust-about.png" class="about-pic" alt="picture with rusty text saying About.">
-				<div class="section-about-text">
-					<p>Hi, I’m Anders Gustavsson. This is my portfolio where i share all of my projects. When im not designing web pages in my free time, im studying for Front End Development. Outside of school and work i like to, playing video games, work out at the gym, hanging out with my friends, eat good food. I also like to make a trip atleast once a year. My first year in school is now complete and have now started my intership as a Front End Developer.</p>
+	</div>
+	<div class="background-section">
+		<div class="section-about" id="About">
+			<img src="./assets/images/rust-about.png" class="about-pic" alt="picture with rusty text saying About.">
+			<div class="section-about-text">
+				<p>Hi, I’m Anders Gustavsson. This is my portfolio where i share all of my projects. When im not designing web pages in my free time, im studying for Front End Development. Outside of school and work i like to, playing video games, work out at the gym, hanging out with my friends, eat good food. I also like to make a trip atleast once a year. My first year in school is now complete and have now started my intership as a Front End Developer.</p>
 
-					<p>So far in school I have completed classes in HTML & CSS, PHP programming, MySQL, graffic designer tool: Photoshop and JavaScript.</p>
+				<p>So far in school I have completed classes in HTML & CSS, PHP programming, MySQL, graffic designer tool: Photoshop and JavaScript.</p>
 
-					<p>Right now im doing my first intership at a financial company called Relenda AB as a <a href="https://angular.io/guide/quickstart" class="angular-link">Angular</a> developer. So far our team has developt a beta plattform which contains services such as to be able to sell your own and finance others eDI-invoices.</p>
+				<p>Right now im doing my first intership at a financial company called Relenda AB as a <a href="https://angular.io/guide/quickstart" class="angular-link">Angular</a> developer. So far our team has developt a beta plattform which contains services such as to be able to sell your own and finance others eDI-invoices.</p>
 
-					<p>Other assignments i have been tasked with are for example: design and create the companys new logo for the new plattform and also designed a product sheet for plattforms services with Photoshop.</p>
-				</div>
-				<img src="./assets/images/profile-pic.jpg" class="profile-pic" alt="picture of the owner of the website. White male, beard, in his thirties.">	
+				<p>Other assignments i have been tasked with are for example: design and create the companys new logo for the new plattform and also designed a product sheet for plattforms services with Photoshop.</p>
 			</div>
-			<div class="contact-section-background" id="Contact">
-				<img src="./assets/images/rust-contact.png">
-				<div class="section-contact">
-					<!-- <div class="contact-info-container"> -->
-						<p>I am looking for work after i have finished my studies. So if you wish to contact me about a job offer or a private project let me know. </p>
+			<img src="./assets/images/profile-pic.jpg" class="profile-pic" alt="picture of the owner of the website. White male, beard, in his thirties.">	
+		</div>
+		<div class="contact-section-background" id="Contact">
+			<img src="./assets/images/rust-contact.png">
+			<div class="section-contact">
+				<!-- <div class="contact-info-container"> -->
+					<p>I am looking for work after i have finished my studies. So if you wish to contact me about a job offer or a private project let me know. </p>
 
-						<p>You can reach me either by phone or email.</p>
-						<!-- <div class="contact-container"> -->
-							<ul class="contact-list">
-								<li class="contact-item"><a href="tel:0768505328">Tel: 0768505328</a></li>
-								<li class="contact-item"><a href="mailto: gustavsson09@hotmail.com">E-mail: gustavsson09@hotmail.com</a></li>
-							</ul>
-						<!-- </div> -->
+					<p>You can reach me either by phone or email.</p>
+					<!-- <div class="contact-container"> -->
+						<ul class="contact-list">
+							<li class="contact-item"><a href="tel:0768505328">Tel: 0768505328</a></li>
+							<li class="contact-item"><a href="mailto: gustavsson09@hotmail.com">E-mail: gustavsson09@hotmail.com</a></li>
+						</ul>
 					<!-- </div> -->
-					<!-- <div class="link-container">
-						<img src="http://cleaner-and-launderer.com/wp-content/uploads/2014/11/linkedin.png" alt="" class="linkedin-icon">
-						<img src="http://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19761.png" alt="" class="facebook-icon">
-					</div> -->
-				</div>
+				<!-- </div> -->
 			</div>
-			<!-- <div class="section-about-top-border" id="Projects">
-				
-			</div> -->
-			<div class="section-projects" id="Projects">
-				<img src="./assets/images/rust-projects.png">
-				<div class="section-projects-text">
-					<p>This is my portfolio where i share all of my projects. </p>
-				</div>
-				<section class="section-gallery">
-			        <ul id="gallery">
-			          <li>
+		</div>
+		<div class="section-projects" id="Projects">
+			<img src="./assets/images/rust-projects.png">
+			<div class="section-projects-text">
+				<p>This is my portfolio where i share all of my projects. </p>
+			</div>
+			<section class="section-gallery">
+		        <ul id="gallery">
+		          <li>
+		          	<div class="drop">
 			            <a href="https://wastelandsolution.se/Inl_1/" target="_blank">
 			              <img src="./assets/images/munk1.png" alt="Image of donuts. Schoolproject of a donuts site." class="projects-images">
+			              <div class="drop-content">
 			              <p>My first html and css website i devolopted in school. Not much to look at but it is more for showing progress</p>
+			          	  </div>
 			            </a>
-			          </li>
-			          <li>
+		            </div>
+		          </li>
+		          <li>
+		          	<div class="drop">
 			            <a href="./assets/images/inl_2.jpg">
 			              <img src="./assets/images/inl_2.jpg" alt="image of website. Website name: Weyland Yutani" class="projects-images">
-			              <p>Schoolproject. One of my first websites. Developed basic database system with PHP programming in school. Alien universe inspired. Only placeholder images since server is not up and running</p>
+			              <div class="drop-content">
+			              	<p>Schoolproject. One of my first websites. Developed basic database system with PHP programming in school. Alien universe inspired. Only placeholder images since server is not up and running.
+			              	</p>
+			          	  </div>
 			            </a>
-			          </li>
-			          <li>
+		        	</div>
+		          </li>
+		        </ul>
+		        <ul id="gallery">
+		          <li>
+		          	<div class="drop">
 			            <a href="./assets/images/inl_3.jpg">
 			              <img src="./assets/images/inl_3.jpg" alt="Website of a to do list." class="projects-images">
-			              <p>Schoolproject. Developed a ToDo list with php programming. Star Craft inspired. Only placeholder images since server is not up and running</p>
+			              <div class="drop-content">
+			              	<p>Schoolproject. Developed a ToDo list with php programming. Star Craft inspired. Only placeholder images since server is not up and running.
+			              	</p>
+			          	  </div>
 			            </a>
-			          </li>
-			          <li>
+		        	</div>
+		          </li>
+		          <li>
+		          	<div class="drop">
 			            <a href="https://wastelandsolution.se/javascript/svg/">
 			              <img src="./assets/images/firewall-shield.png" alt="Image of a shield representing a infographic" class="projects-images">
-			              <p>A infographic for promoting SSL cerificate made with greensock js and Javascript</p>
+			              <div class="drop-content">
+			              	<p>A infographic for promoting SSL cerificate made with greensock js and Javascript</p>
+			              </div>
 			            </a>
-			          </li>
-			          <li>
+		        	</div>
+		          </li>
+		        </ul>
+		        <ul id="gallery">
+		          <li>
+		          	<div class="drop">
 			            <a href="https://wastelandsolution.se/javascript/quizz/" target="_blank">
 			              <img src="./assets/images/quizz.png" alt="Image of quizz site. Big alien" class="projects-images">
-			              <p>My first JavaScript quizz.</p>
+			              <div class="drop-content">
+			              	<p>My first JavaScript quizz.</p>
+			          	  </div>
 			            </a>
-			          </li>
-			          <li>
+		        	</div>
+		          </li>
+		          <li>
+		          	<div class="drop">
 			            <a href="https://www.rvft.se/" target="_blank">
 			              <img src="./assets/images/rvft.png" alt="Image of a company startingpage. Company RVFT." class="projects-images">
-			              <p>My first live project for a company were i devolopted their site with wordpress and custom made wordpresstheme.</p>
+			              <div class="drop-content">
+			              	<p>My first live project for a company were i devolopted their site with wordpress and custom made wordpresstheme.</p>
+			          	  </div>
 			            </a>
-			          </li>
-			          <li>
+		        	</div>
+		          </li>
+		        </ul>
+		        <ul id="gallery">
+		          <li>
+		          	<div class="drop">
 			            <a href="https://wastelandsolution.se/javascript/weatherapp/" target="_blank">
 			              <img src="./assets/images/weatherapp.png" alt="Image of a weatherapp browser site" class="projects-images">
-			              <p>Schoolproject in a group of three developted our own weahterapp for browser. Using javascript and three diffrent api:s that are fetching weather data, geoloction and images.</p>
+			              <div class="drop-content">
+			              	<p>Schoolproject in a group of three developted our own weahterapp for browser. Using javascript and three diffrent api:s that are fetching weather data, geoloction and images.</p>
+			          	  </div>
 			            </a>
-			          </li>
-			          <li>
+		        	</div>
+		          </li>
+		          <li>
+		          	<div class="drop">
 			            <a href="https://wastelandsolution.se/bulldad/index.php" target="_blank">
 			              <img src="./assets/images/bulldad.png" alt="picture of a website: Cult of bulldada. Schoolproject" class="projects-images">
-			              <p>Schoolproject were which we were tasked with develop a fictive site for a fictive cult. The assignment was to practice design and UX and Usability</p>
+			              <div class="drop-content">
+			              	<p>Schoolproject were which we were tasked with develop a fictive site for a fictive cult. The assignment was to practice design and UX and Usability</p>
+			          	  </div>
 			            </a>
-			          </li>
-			        </ul>
-			    </section>
-			</div>	
-		</div>
+		        	</div>
+		          </li>
+		        </ul>
+		    </section>
+		</div>	
 	</div>
 </main>
 <?php include_once "./templates/footer.php" ?>
